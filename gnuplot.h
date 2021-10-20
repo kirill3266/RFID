2e267236642c4ac5a3124106632a9be317c69b8a
Афанасьев Кирилл <ka.kirafa@rambler.ru>
2021-10-20 11:28:06 +0300

    Added gnuplot.cpp, removed extra source dir

--- gnuplot.h
+++ gnuplot.h
@@ -6,7 +6,7 @@
 #include <iostream>
 
 #define GNUPLOT_NAME "gnuplot -persist"
-#define DIR "C:\\Users\\kakir\\CLionProjects\\article"
+#define DIR "YOUR_DIR"
 
 class Gnuplot {
 public:
@@ -20,32 +20,5 @@ protected:
     FILE *gnuplotpipe;
 };
 
-Gnuplot::Gnuplot() {
-#ifdef WIN32
-    gnuplotpipe = _popen(GNUPLOT_NAME, "w");
-    fprintf(gnuplotpipe, "cd '%s'\n", DIR);
-#else
-    gnuplotpipe  = popen(GNUPLOT_NAME, "w");
-#endif
-
-    if (!gnuplotpipe) {
-        std::cerr << ("Gnuplot not found !");
-    }
-}
-
-Gnuplot::~Gnuplot() {
-    fprintf(gnuplotpipe, "exit\n");
-
-#ifdef WIN32
-    _pclose(gnuplotpipe);
-#else
-    pclose(gnuplotpipe);
-#endif
-}
-
-void Gnuplot::operator()(const std::string &command) {
-    fprintf(gnuplotpipe, "%s\n", command.c_str());
-    fflush(gnuplotpipe); //without fflush nothing will draw
-};
 
 #endif
